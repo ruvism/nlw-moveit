fac19ea322a8826014cb5bd1d1d6f4aa194911d5
ruvism <ruvila.m@gmail.com>
2021-03-04 09:34:38 -0300

    Primeiro commit do projeto

---
+++ src/components/ChallengeBox.tsx
@@ -0,0 +1,63 @@
+import styles from '../styles/components/ChallengeBox.module.css';
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import { CountdownContext } from '../contexts/CountdownContext';
+
+
+export function ChallengeBox(){
+    const { activeChallenge, resetChallenge, completeChallenge } = useContext(ChallengesContext);
+    const { resetCountdown } = useContext(CountdownContext);
+
+    function handleChallengeFailed(){
+        resetChallenge();
+        resetCountdown ();
+    }
+    function handleChallengeSucceed (){
+        completeChallenge();
+        resetCountdown ();
+    }
+    
+    return (
+       <div className={styles.challengeBoxContainer}>
+           { activeChallenge ? (
+            <div className={styles.challengeActive}>
+                <header>Ganhe {activeChallenge.amount}xp</header>
+                <main>
+                    <img src={`icons/${activeChallenge.type}.svg`} alt="braço levantando peso"/>
+                    <strong>Novo desafio</strong>
+                    <p> { activeChallenge.description } </p>
+                </main>
+                <footer>
+                    <button 
+                        type="button"
+                        className={styles.challengeFailedButton}
+                        onClick={ handleChallengeFailed }
+                    >
+                        Falhei
+                    </button>
+                    <button 
+                     type="button"
+                     className={styles.challengeSucceededButton}
+                     onClick={ handleChallengeSucceed }
+                    >
+                        Completei
+                    </button>
+
+                </footer>
+                
+            </div>
+
+           ) : (
+            <div className={styles.challengeNotActive}>
+                <strong>Finalize um ciclo para receber novos desafios</strong>
+                <p>
+                    <img src="icons/level-up.svg" alt="level up"/>
+                    Avance de level ao completar ciclos de exercícios
+                </p>
+            </div>
+
+           )}
+
+       </div>
+    );
+}
---
+++ src/pages/_document.tsx
@@ -0,0 +1,19 @@
+import Document, { Html, Head, Main, NextScript} from 'next/document'
+
+export default class MyDocument extends Document {
+ render(){
+     return(
+        <Html>
+            <Head>
+                <link rel="shortcut icon" href="favicon.png" type="image/png"/>
+            {/* fontes */}
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+
+        </Html>
+     )
+ }   
+}
